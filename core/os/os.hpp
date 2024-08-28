91713f02c8a31fa03723c59e01ebfa458e38230f
Adam Spivack <spivack@outlook.com>
2024-08-28 13:40:33 -0700

    Fleshing out the OS specific loading system

---
+++ core/os/os.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace os
+{
+#ifdef _WIN64
+    extern void game_main();
+    void create_window();
+    void create_console();
+#endif
+
+}
---
+++ core/os/windows/os.cpp
@@ -0,0 +1,13 @@
+#define WIN32_LEAN_AND_MEAN
+
+#include <Windows.h>
+#include "Window.h"
+
+namespace os
+{
+
+    extern void game_main();
+    void create_window();
+    void create_console();
+
+}
